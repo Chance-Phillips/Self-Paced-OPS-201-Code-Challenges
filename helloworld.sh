336e663dcf12eabfaaceae3eb75a4c693c8081a9
Chance-Phillips <m.chancephillips18@gmail.com>
2024-01-27 11:58:18 -0800

    new code

---
+++ helloworld.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Script Name:                  helloworld.sh
+# Author:                       Chance Phillips
+# Date of latest revision:      01/27/2024
+# Purpose:                      Automate run "helloworld"
+
+# Declaration of variables
+
+# Declaration of functions
+
+
+# Main
+echo "Hello World!"
